d727be27b75774542aea3eab56dd23eae0821625
Lucas Ievenes Ratti <61846972+Ievenes@users.noreply.github.com>
2025-03-18 20:34:55 -0300

    Aula 4
    Criação dos fakers

--- Aula04_18Mar/fakers.js
+++ Aula04_18Mar/fakers.js
@@ -1,5 +1,7 @@
 import nomes from "./fakedata/nomes.js"
 import sobrenomes from "./fakedata/sobrenomes.js"
+import rg from "./fakedata/rg.js"
+import email from "./fakedata/email.js"
 
 function fakeName() {
     let num = parseInt(Math.random()*nomes.length)
@@ -11,17 +13,34 @@ function fakeSobrenome() {
     return sobrenomes[num]
 }
 
+function fakePassword() {
+    let num = parseInt(Math.random()*1000000)
+    return num.toString()
+}
+
+function fakeRg() {
+    return rg()
+}
+
+function fakeEmail() {
+    let num = parseInt(Math.random()*email.length)
+    return email[num]
+}
+
 export function fakeUser() {
     let firstName = fakeName()
     let lastName = fakeSobrenome()
     let username = firstName.charAt(0)+lastName
     username = username.toLowerCase()
+    let password = fakePassword()
+    let rg = fakeRg()
+    let email = username+"@"+fakeEmail()
     return {
         firstName,
         lastName,
         username,
-        password: "",
-        rg: "",
-        email: ""    
+        password,
+        rg,
+        email  
     }
 }
